ee0a75898c84a3efb49bfd10edcf089172cee262
rdslauder <robertdslauder@gmail.com>
2023-04-23 02:26:21 +0100

    Rename scripts
    2 and 3 - Some batch renaming scripts previously done
    0 - a new script to rename images according to the time/ date they were taken

---
+++ 1_Rename/3_BatchRenameImages#.py
@@ -0,0 +1,33 @@
+import os
+
+# Get script directory
+script_directory = os.path.dirname(os.path.realpath(__file__))
+
+# Set the path to the directory containing the files to be renamed
+folder_path = os.path.join(script_directory)
+
+# Prompt the user to input a prefix
+batch_prefix = input("Enter the batch number: ")
+
+# Set the starting index
+start_index = 1
+
+# Loop through each file in the directory
+for i, filename in enumerate(os.listdir(folder_path)):
+    # Check if the file is an image
+    if os.path.splitext(filename)[1].lower() in ['.jpg', '.jpeg', '.png', '.bmp', '.gif']:
+        # Set the new filename
+        new_filename = batch_prefix + '#' + str(start_index) + '.jpg'
+
+        # Set the paths to the source and destination files
+        source_path = os.path.join(folder_path, filename)
+        destination_path = os.path.join(folder_path, new_filename)
+
+        # Rename the file
+        os.rename(source_path, destination_path)
+
+        # Increment the index
+        start_index += 1
+
+# Print a message indicating that the operation is complete
+print("All image files in the directory have been renamed with the batch number '" + prefix + "'#")
